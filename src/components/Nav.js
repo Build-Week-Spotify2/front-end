dff4828823ebf0b75db3b2631be305bacf287fa9
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-08-20 20:51:07 -0600

    nav bar set up & styled just need to feed dynamic username in and add sign out functinality later down the road

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,17 +3,46 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 
 const NavContainer = styled.div`
-    border: 1px solid black;
     margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 75px;
+    background-image: linear-gradient(black, #666666);
+`
+
+const NavItem = styled.div`
+    width: 95px;
+    font-weight: 700;
+    height: 20px;
+    text-align: center;
+    
+    margin: 5px;
+    padding: 5px;
+    border-radius: 10px;
+    background-color: white;
+
+    &:hover {
+        background-color: #1DB954;
+    }
+`
+
+const NavUser = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    right: 2%;
+    text-align: center;
+
 `
 
 const NavBar = () => {
     return(
         <NavContainer>
-            <div><Link to='/dashboard'>Home</Link></div>
-            <div><Link to='/saved-songs'>Saved Songs</Link></div>
-            <div><Link to='/search-songs'>Search</Link></div>
-            <div><Link to='/'>Sign Out</Link></div>
+            <NavItem><Link to='/dashboard'>Home</Link></NavItem>
+            <NavItem><Link to='/saved-songs'>Saved Songs</Link></NavItem>
+            <NavItem><Link to='/search-songs'>Search</Link></NavItem>
+            <NavUser><div>Username</div><div>Sign Out</div></NavUser>
         </NavContainer>
     )
 }
