6a2d81deee7ab135f7e28b409e9aaf1e68e770f1
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-08-20 19:21:51 -0600

    base router set up with empty components we know we will need

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import SignIn from './components/SignIn';
+import Dashboard from './components/Dashboard';
+import SavedSongs from './components/SavedSongs';
+import Search from './components/Search';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
-    <p>App</p>
-  );
+  return (<>
+    <Nav/>
+
+    <Switch> 
+      <Route exact path='/' component={SignIn} />
+      <Route path='/dashboard' component={Dashboard} />
+      <Route path='/saved-songs' component={SavedSongs} />
+      <Route path='/search-songs' component={Search} />
+    </Switch>
+   
+  </>);
 }
 
 export default App;
---
+++ src/components/SignIn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignIn = () => {
+    return(<>
+        <p>Sign in Will Go here</p>
+    </>)
+}
+
+export default SignIn;
