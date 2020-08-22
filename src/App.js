c108f4952e88f20d9fa95dbeb7d810f2031f4b00
Tylar Washington-Beechum <tylay47@gmail.com>
2020-08-21 22:55:55 -0400

    I need some help with the formSchema function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Dashboard from './components/Dashboard';
 import SavedSongs from './components/SavedSongs';
 import Search from './components/Search';
 import { Route, Switch } from 'react-router-dom';
+import "./components/styles.css";
 
 function App() {
   return (<>
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,112 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import * as yup from "yup";
+import styled from 'styled-components'
+
 
 const SignIn = () => {
+  
+const [formState, setFormState] = useState({
+    email: "",
+    password:"",
+    
+})
+
+const [errors, setErrors] = useState({
+    email: "", 
+    password: ""
+})
+
+
+const formSubmit = (e) => {
+    e.preventDefualt();
+    console.log("form submitted")
+}
+
+const inputChange = (e) => {
+    console.log("input changed", e.target.value);
+    const newFormData = {
+        ...formState,
+        [e.target.name]:e.target.value
+    }
+    validateChange(e.target);
+    setFormState(newFormData)
+}
+
+
+const formSchema = yup.object().shape({
+    email: yup
+    .string()
+    .email("Must be a vaild email")
+    .required("Must include an email"),
+    password:yup
+    .string()
+    .required("Password is a required field")
+    .password("Must be a vaild password")
+
+});
+
+ const validateChange = (e) => {
+ console.log(e)
+    yup  
+       .reach(formSchema, e.name)
+       .then((vaild) => {
+      setErrors({
+          ...errors,
+          [e.name]:""      
+        });
+    })
+   .catch((err) => {
+       console.log(err);
+
+       setErrors({
+           ...errors,
+           [e.name]: err.errors[0]
+       });
+   });
+
+};
+
+
+
+
+
+
+
+
+
+
     return(<>
-        <p>Sign in Will Go here</p>
+      <form onSubmit={formSubmit}>
+
+
+<label htmlFor="email">
+    Email
+    <input
+    id="email"
+    type="text"
+    name="email"
+    value={formState.email}
+    onChange={inputChange}
+    />
+    
+</label>
+
+<label htmlFor="password">
+    Password (15 characters minimum):
+    <input
+    id="password"
+    type="password"
+    name="password"
+    value={formState.password}
+    minLength="6" required
+    onChange={inputChange}
+    />
+   
+</label>
+
+
+<button  type="submit">Submit</button>
+    </form>
     </>)
 }
 
